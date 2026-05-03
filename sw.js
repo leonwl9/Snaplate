8755055881d1f685ed4bdde6a21a489a56dade27
Turgut Dal <111610461+turgut5@users.noreply.github.com>
2026-05-03 20:17:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kalori-app-v1';
+const CACHE_NAME = 'kalori-pro-v2';
 const assets = [
   './',
   './index.html',
@@ -8,6 +8,7 @@ const assets = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
+      console.log('Dosyalar önbelleğe alınıyor...');
       return cache.addAll(assets);
     })
   );
@@ -16,6 +17,7 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
+      // Önbellekte varsa onu ver, yoksa internetten çek
       return response || fetch(event.request);
     })
   );
